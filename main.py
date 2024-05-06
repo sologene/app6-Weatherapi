c17b15e73ec7daa5351618e9f6e53a28ea2398c9
Sologene <basil.s.maliyil@gmail.com>
2024-05-06 20:09:55 +0800

    6/5/24: added more url endpoints

--- main.py
+++ main.py
@@ -2,19 +2,34 @@ from flask import Flask, render_template
 import pandas as pd
 
 app = Flask("__name__")
-
+stations = pd.read_csv("data_small/stations.txt", skiprows=17)
+stations = stations[["STAID", "STANAME"]]
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
-def about(station,date):
+def about(station, date):
     df = pd.read_csv("data_small/TG_STAID"+str(station).zfill(6)+".txt", skiprows=20, parse_dates=["    DATE"])
     temprature = df.loc[df["    DATE"] == date]["   TG"].squeeze()/10
     return {"station": station,
             "date":date,
             "temprature":temprature}
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    df = pd.read_csv("data_small/TG_STAID"+str(station).zfill(6)+".txt", skiprows=20, parse_dates=["    DATE"])
+    return df.to_dict(orient="records")
+
+@app.route("/api/yearly/v1/<station>/<year>")
+def all_year(station,year):
+    df = pd.read_csv("data_small/TG_STAID"+str(station).zfill(6)+".txt", skiprows=20)
+    df["    DATE"]= df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
