c1d75b8f5f29036032ecbd9a82db67751861f5ba
Sologene <basil.s.maliyil@gmail.com>
2024-05-06 12:08:32 +0800

    6/5/24: Returns temprature for any station and date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import pandas as pd
+
 app = Flask("__name__")
 
 @app.route("/")
@@ -9,8 +10,8 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station,date):
-    df = pd.read_csv("")
-    temprature = df
+    df = pd.read_csv("data_small/TG_STAID"+str(station).zfill(6)+".txt", skiprows=20, parse_dates=["    DATE"])
+    temprature = df.loc[df["    DATE"] == date]["   TG"].squeeze()/10
     return {"station": station,
             "date":date,
             "temprature":temprature}
